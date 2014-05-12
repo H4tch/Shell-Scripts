2bf7c1685947460cdd13f4025a80faadb5735c9e
Dan Hatch <danhatch333@gmail.com>
2014-05-11 23:45:04 -0500

    Added instructions to record-audio.sh to find alsa output.

--- record-audio.sh
+++ record-audio.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+## You'll need to replace alsa_output.pci..... to match your card.
+## To find your card's name, type "pactl list". Search for the Source sections
+## until you find an output source. You'll probably want to avoid "hdmi" sources
+## if you are on a laptop.
 avconv \
-        -f pulse -ac 2 -i alsa_output.pci-0000_00_14.2.analog-stereo.monitor -acodec libmp3lame \
+        -f pulse -ac 2 -i alsa_output.pci-0000_00_1b.0.analog-stereo.monitor -acodec libmp3lame \
         -threads 2 $1
 
